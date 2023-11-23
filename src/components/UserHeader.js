1dd58d8453e5f2a8e2669cc11def40058466898f
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-23 16:46:36 +0100

    a

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,64 +1,79 @@
-import { Box, Flex, VStack, Text, Link } from "@chakra-ui/layout";
-import { Avatar } from "@chakra-ui/react";
-import {Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu"
+import { Box, Flex, Text, VStack, Link } from "@chakra-ui/layout";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import { Portal } from "@chakra-ui/portal";
-import { CgMoreO } from "react-icons/cg";
+import { Avatar } from "@chakra-ui/react";
 import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+import { useToast } from '@chakra-ui/toast'
 
 const UserHeader = () => {
-  return (
-    <VStack gap={4} alignItems={"start"}>
-      <Flex justifyContent={"space-between"} w={"full"}>
-        <Box>
-          <Text fontSize={"2xl"} fontWeight={"bold"}>
-            Alimi Muaz .O
-          </Text>
-          <Flex gap={2} align={"center"}>
-            <Text fontSize={"sm"}>yungalimzy123</Text>
-            <Text
-              fontSize={"xm"}
-              bg={"gray.dark"}
-              color={"gray.light"}
-              p={1}
-              borderRadius={"full"}
-            >
-              thread.net
-            </Text>
-          </Flex>
-        </Box>
 
-        <Box>
-          <Avatar name="Alimi Muaz" src="/directorPro.jpeg" size={"xl"} />
-        </Box>
-      </Flex>
+    const toast = useToast()
 
-      <Text>FrontEnd developer of DltAfrica Chort 3.0</Text>
-      <Flex w={"full"} justifyContent={"space-between"}>
-        <Flex gap={2} alignItems={"center"}>
-          <Text color={"gray.light"}>4.5m followers</Text>
-          <Box w={1} h={1} bg={"gray.light"} borderRadius="50%"></Box>
-          <Link color={"gray.light"}>instagram.com</Link>
-        </Flex>
-        <Flex gap={2}>
-          <Box className="icon-container">
-            <BsInstagram size={24} cursor={"pointer"} />
-          </Box>
-          <Box className="icon-container">
-            <Menu>
-                <MenuButton>
-                    <CgMoreO size={24} cursor={"pointer"} />
-                </MenuButton>
-                <Portal>
-                     <MenuList bg={"gray.dark"}>
-                        <MenuItem bg={"gray.dark"}>Click here</MenuItem>
-                     </MenuList>
-                </Portal>
-            </Menu>
-          </Box>
-        </Flex>
-      </Flex>
-    </VStack>
-  );
+    const copyURL = () => {
+        const currentURL = window.location.href;
+        navigator.clipboard.writeText(currentURL).then(() => {
+            toast({
+                title: "URL Copied",
+                description: "Profile link copied",
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+            })
+        })
+    }
+
+   return (
+      <VStack gap={4} alignItems={"start"}>
+         <Flex justifyContent={"space-between"} w={"full"}>
+            <Box>
+               <Text fontSize={"2xl"} fontWeight={"bold"}>
+                  Aliu Musa
+               </Text>
+               <Flex gap={2} alignItems={"center"}>
+                  <Text fontSize={"sm"}>aliumusa@99</Text>
+                  <Text
+                     fontSize={"xs"}
+                     bg={"gray.dark"}
+                     color={"gray.light"}
+                     p={1}
+                     borderRadius={"full"}
+                  >
+                     threads.net
+                  </Text>
+               </Flex>
+            </Box>
+            <Box>
+               <Avatar name="Aliu Musa" src="/directorPro.jpeg" size={"xl"} />
+            </Box>
+         </Flex>
+         <Text>Founder, executive chairman and CEO of DLTAfrica</Text>
+         <Flex w={"full"} justifyContent={"space-between"}>
+            <Flex gap={2} alignItems={"center"}>
+               <Text color={"gray.light"}>3.2k followers</Text>
+               <Box w={1} h={1} bg={"gray.light"} borderRadius={"50%"}></Box>
+               <Link color={"gray.light"}>instagram.com</Link>
+            </Flex>
+            <Flex gap={2}>
+               <Box className="icon-container">
+                  <BsInstagram size={24} cursor={"pointer"}/>
+               </Box>
+               <Box className="icon-container">
+                <Menu>
+                    <MenuButton>
+                      <CgMoreO size={24} cursor={"pointer"}/>
+                    </MenuButton>
+                    <Portal>
+                        <MenuList bg={"gray.dark"}>
+                            <MenuItem color={"white"} bg={"gray.dark"} onClick={copyURL}>Click here</MenuItem>
+                        </MenuList>
+                    </Portal>
+                </Menu>
+               </Box>
+            </Flex>
+         </Flex>
+      </VStack>
+   );
 };
 
 export default UserHeader;
