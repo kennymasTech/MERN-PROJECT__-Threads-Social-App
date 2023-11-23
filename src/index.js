9b85e091ab937c5845de9f44bbda5626c2d30428
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-23 12:17:01 +0100

    n

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools'
 import { extendTheme } from '@chakra-ui/theme-utils'
 import { ColorModeScript } from '@chakra-ui/color-mode'
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -36,10 +37,12 @@ const theme = extendTheme({config, styles, colors})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <ChakraProvider theme={theme}>
-       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-          <App />
-      </ChakraProvider>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+              <App />
+        </ChakraProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
