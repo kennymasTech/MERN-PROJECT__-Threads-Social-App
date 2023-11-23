c7689d5d2aafae226d084139983f854c1b7cf6d8
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-23 16:05:12 +0100

    d

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import Header from './components/Header';
+import { Container } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import UserPage from './pages/UserPage';
+import PostPage from './pages/PostPage'
+
+
+
+function App() {
+
+
+  return (
+    <Container maxW="620px"> 
+    <Header />
+    <Routes>
+      <Route path="/:username" element={<UserPage />}/>
+      <Route path="/:username/post/:pid" element={<PostPage />}/>
+    </Routes>
+  
+
+    </Container>
+ 
+  );
+}
+
+export default App;
---
+++ src/components/UserHeader.js
@@ -0,0 +1,16 @@
+import { Box, Flex, VStack, Text } from "@chakra-ui/layout"
+
+const UserHeader = () => {
+
+  return  <VStack gap={4} alignItems={"start"}>
+    <Flex justifyContent= {"space-between"} w={"full"}>
+        <Box>
+            <Text>Aliu Musa</Text>
+        </Box>
+    </Flex>
+
+  </VStack>
+
+}
+
+export default UserHeader
