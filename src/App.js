5c2b6de23918974a017aed3bc5b2a112857c1418
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-23 17:37:55 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-
-
-import { Container } from '@chakra-ui/react'
-import { Routes, Route } from 'react-router-dom'
-import UserPage from './pages/UserPage'
-import PostPage from './pages/PostPage'
-import Hearder from './components/Hearder'
+import { Container } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+import Hearder from "./components/Hearder";
 
 const App = () => {
   return (
-    <Container  maxW="620px">
-        <Hearder/>
+    <Container maxW="620px">
+      <Hearder />
       <Routes>
-        <Route path='/:username' element={<UserPage />}/>
-        <Route path='/:username/post/:pid' element={<PostPage />} />
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
-       
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
