627f8404101859b4b9027f93e3b1598116d7ef64
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-23 11:37:49 +0100

    a

--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/theme-utils'
+import { ColorModeScript } from '@chakra-ui/color-mode'
+
+
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("grey.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#101010")(props)
+    }
+  })
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider>
+          <App />
+      </ChakraProvider>
   </React.StrictMode>
 );
 
