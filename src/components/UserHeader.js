7c6a43d86d552480ae5161129fb960d053cfc558
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-25 16:57:53 +0100

    a

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -9,7 +9,6 @@
 
 
 
-
 import { Box, Flex, Text, VStack, Link } from "@chakra-ui/layout";
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import { Portal } from "@chakra-ui/portal";
