250b29f6f69ea9cb59d6926399c2c275a560c358
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-27 10:28:17 +0100

    a

--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -1,12 +1,13 @@
 
-import { Avatar, Flex } from "@chakra-ui/react";
+import { Avatar, Box, Flex } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const UserPosts = () => {
   return <Link to={"/aliumusa/post/1"}>
     <Flex gap={3} mb={4} py={5}>
         <Flex>
-            <Avatar src="/post1.png" name="Mark Zuckerberg"/>
+            <Avatar src="/post1.png" name="Mark Zuckerberg" size={'md'}/>
+            <Box w={1} h={'full'} bg={'gray.light'} my={2}></Box>
         </Flex>
     </Flex>
   </Link>;
