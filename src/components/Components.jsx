36293e71aa51cedc3791c41bcb086fd575b152ef
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-27 15:53:46 +0100

    z

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Avatar, Flex } from "@chakra-ui/react";
+import React from "react";
 
-const Components = () => {
+const Components = ({ comment, createdAt, likes, username, userAvatar }) => {
   return (
-    <div>Components</div>
-  )
-}
+    <React.Fragment>
+      <Flex gap={4} my={2} py={2} w={"full"}>
+        <Avatar
+          src="https://bit.ly/ryan-florence"
+          size={"sm"}
+          name="Ryan Florence"
+        />
+      </Flex>
+    </React.Fragment>
+  );
+};
 
-export default Components
+export default Components;
