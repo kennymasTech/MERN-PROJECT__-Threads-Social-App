7a68705cffffcd7565551043a74739913ac2da44
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-27 14:18:58 +0100

    m

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,16 +1,30 @@
-
 import UserHeader from "../components/UserHeader";
 import UserPosts from "../components/UserPosts";
 
 const UserPage = () => {
-   return (
-      <>
-         <UserHeader/>
-         <UserPosts likes={200} replies={70} postImg={'/post1.png'} postTitle={'Hello Kennymas'}/>
-         <UserPosts likes={100} replies={120} postImg={'/post1.png'} postTitle={'This is Great'}/>
-         <UserPosts likes={50} replies={250} postImg={'/post1.png'} postTitle={'Wow! Awesome'}/>
-      </>
-   );
+  return (
+    <>
+      <UserHeader />
+      <UserPosts
+        likes={200}
+        replies={70}
+        postImg={"/post1.png"}
+        postTitle={"Hello Kennymas"}
+      />
+      <UserPosts
+        likes={100}
+        replies={120}
+        postImg={"/post1.png"}
+        postTitle={"This is Great"}
+      />
+      <UserPosts
+        likes={50}
+        replies={250}
+        postImg={"/post1.png"}
+        postTitle={"Wow! Awesome"}
+      />
+    </>
+  );
 };
 
 export default UserPage;
