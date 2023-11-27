c6a9ec2b8c7cda18fcbfcf397468950a4d1a5631
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-27 10:14:26 +0100

    a

--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -6,7 +6,7 @@ const UserPosts = () => {
   return <Link to={"/aliumusa/post/1"}>
     <Flex gap={3} mb={4} py={5}>
         <Flex>
-            <Avatar src="/post1.png"/>
+            <Avatar src="/post1.png" name="Mark Zuckerberg"/>
         </Flex>
     </Flex>
   </Link>;
