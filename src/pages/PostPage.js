5768c5414483431ebf740c5ff7a84e29c917e96a
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-27 15:01:09 +0100

    m

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,17 +1,39 @@
-
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { Avatar, Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { BsThreeDots } from "react-icons/bs";
 
 const PostPage = () => {
   return (
     <>
       <Flex>
-        <Flex>
-          
+        <Flex
+          w={"full"}
+          gap={3}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} />
+
+          <Flex alignItems={"center"}>
+            <Text fontSize={"sm"}>Aliu Musa</Text>
+            <Image src="/verified.png" h={4} w={4} ml={2} />
+          </Flex>
+
+          <Flex gap={4} alignItems={"center"}>
+            <Text
+              fontSize={{ base: "xs", md: "sm"}}
+              textAlign={"right"}
+              width={36}
+              color={"gray.light"}
+            >
+              2day
+            </Text>
+            <BsThreeDots />
+          </Flex>
         </Flex>
       </Flex>
     </>
-  )
+  );
 };
 
 export default PostPage;
