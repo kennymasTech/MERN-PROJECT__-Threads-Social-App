3dd8673b8f391e026cf1bce9d87e3717626ab6d1
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-27 10:07:50 +0100

    a

--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -1,7 +1,15 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Flex } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
 
 const UserPosts = () => {
-  return <Link to={"/aliumusa/post/1"}></Link>;
+  return <Link to={"/aliumusa/post/1"}>
+    <Flex>
+        <Flex>
+            <Avatar/>
+        </Flex>
+    </Flex>
+  </Link>;
 };
 
 export default UserPosts;
