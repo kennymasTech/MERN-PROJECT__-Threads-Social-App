0237f0ae6db759236f14b5058a85d5b9841ed9bc
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-27 10:40:44 +0100

    v

--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 const UserPosts = () => {
   return <Link to={"/aliumusa/post/1"}>
     <Flex gap={3} mb={4} py={5}>
-        <Flex>
+        <Flex flexDir={'column'}>
             <Avatar src="/post1.png" name="Mark Zuckerberg" size={'md'}/>
-            <Box w={1} h={'full'} bg={'gray.light'} my={2}></Box>
+            <Box w={1} h={'full'} bg={'gray.light'} my={2} ></Box>
         </Flex>
     </Flex>
   </Link>;
