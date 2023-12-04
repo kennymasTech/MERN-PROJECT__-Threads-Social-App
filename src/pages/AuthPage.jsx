41c2385d6877b5818aab1f2b69c3650165d9f35f
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-04 11:34:35 +0100

    a

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import authScreenAtom from '../atoms/authAtom'
 import SignupCard from '../components/SignUpCard'
 import LoginCard from '../components/LoginCard'
-import authScreenAtom from '../atoms/authAtom'
 
 const AuthPage = () => {
-    const authScreenAtom = 
-  return <> "login" ? <LoginCard/> : <SignupCard/> </>
+    const authScreenState = useRecoilValue(authScreenAtom)
+  return <> {authScreenState === "login" ? <LoginCard/> : <SignupCard/> } </>
 }
 
 export default AuthPage
