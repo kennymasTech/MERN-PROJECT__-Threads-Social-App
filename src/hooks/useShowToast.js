a42c84be3026b1d4d93b798b62e9fb932988ffd8
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-04 12:52:35 +0100

    a

--- src/hooks/useShowToast.js
+++ src/hooks/useShowToast.js
@@ -4,5 +4,16 @@ import { useCallback } from "react";
 const useShowToast = () => {
     const toast = useToast()
 
+    const showToast = useCallback(
+        ( title, description, status ) => {
+            toast({
+                title,
+                description,
+                status,
+                duration: 3000,
+                isClosable: true,
+            })
     
+        }
+    )
 }
