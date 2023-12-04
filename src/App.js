c6fd0c059ca6046a41ca651eed7675f3212d5e22
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-04 10:04:46 +0100

    a

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import Hearder from "./components/Hearder";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <Container maxW="620px">
       <Hearder />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
