f3053f6e614928486229f07a791b5a798cc0d0aa
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-06 11:20:19 +0100

    a

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -2,17 +2,25 @@ import { useEffect, useState } from 'react';
 import UserHeader from "../components/UserHeader";
 import UserPosts from "../components/UserPosts";
 import { useParams } from 'react-router-dom';
+import useShowToast from '../hooks/useShowToast';
 
 const UserPage = () => {
   const [ user, setUser ] = useState(null);
   const { username } = useParams();
+  const showToast = useShowToast
 
   useEffect(() => {
     const getUser = async () => {
       try {
         const response = await fetch(`/api/users/profile/${username}`);
         const data = await response.json();
-        console.log(data);
+        
+        if(data.error) {
+          showToast("Error", data.error, "error")
+          return;
+        
+        }
+
       } catch (error) {
         console.log(error);
       }
