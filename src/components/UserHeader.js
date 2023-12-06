1e7c10bafd80b96bda2934b7e94cf0bd0f38f127
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-06 12:07:15 +0100

    a

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -47,7 +47,7 @@ const UserHeader = ({user}) => {
             <Avatar name={user.name} src={user.profilePic} size={{ base: "md", md: "xl" }} />
             )}
           {!user.profilePic && (
-            <Avatar name={user.name} src="https://bit.ly/broken-link" size={{ base: "md", md: "xl" }} />
+            <Avatar name={user.name} src="https://tinyurl.com/5n8m9tca" size={{ base: "md", md: "xl" }} />
           )}
         </Box>
       </Flex>
