fb67cb0916a0a7ec6fbec7337e71b4462150c45a
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-06 23:17:26 +0100

    m

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
