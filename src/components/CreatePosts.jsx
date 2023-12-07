7f5e7ddad4b214f4aeaacd01d2b80074133c960f
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-07 11:53:40 +0100

    a

--- src/components/CreatePosts.jsx
+++ src/components/CreatePosts.jsx
@@ -1,146 +1,167 @@
-import { useRef, useState } from 'react'
-import { AddIcon } from '@chakra-ui/icons'
-import { Button, 
-          Modal,
-          ModalOverlay,
-          ModalContent,
-          ModalHeader,
-          ModalFooter,
-          ModalBody,
-          ModalCloseButton, 
-          useDisclosure,
-          FormControl,
-          Textarea,
-          Text,
-          Input,
-          Flex,
-          Image,
-          CloseButton} from '@chakra-ui/react'
-import usePreviewImg from '../hooks/usePreviewImg'
-import { BsFillImageFill } from 'react-icons/bs'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import userAtom from '../atoms/userAtom'
-import { useParams } from 'react-router-dom'
+import { useRef, useState } from "react";
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  Textarea,
+  Text,
+  Input,
+  Flex,
+  Image,
+  CloseButton,
+} from "@chakra-ui/react";
+import usePreviewImg from "../hooks/usePreviewImg";
+import { BsFillImageFill } from "react-icons/bs";
+import { useRecoilState, useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { postsAtom } from "../atoms/postsAtom";
+import { useParams } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
-
-const MAX_CHAR = 500
+const MAX_CHAR = 500;
 
 const CreatePosts = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [ postText, setPostText ] = useState("")
-  const imageRef = useRef(null)
-  const [ remainingChar, setRemainingChar ] = useState(MAX_CHAR)
-  const { handleImgChange, imgUrl, setImgUrl } = usePreviewImg()
-  const [ loading, setLoading ] = useState(false)
-  const user = useRecoilValue(userAtom)
-
-  // const showToast = useShowToast()
-  const [ posts, setPost ] = useRecoilState(postAtom)
-  const { username } = useParams()
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [postText, setPostText] = useState("");
+  const imageRef = useRef(null);
+  const [remainingChar, setRemainingChar] = useState(MAX_CHAR);
+  const { handleImgChange, imgUrl, setImgUrl } = usePreviewImg();
+  const [loading, setLoading] = useState(false);
+  const user = useRecoilValue(userAtom);
+  const showToast = useShowToast();
+  const [posts, setPost] = useRecoilState(postsAtom);
+  const { username } = useParams();
 
   const handleTextChange = (e) => {
-      const inputText = e.target.value
-      if(inputText.length > MAX_CHAR) {
-        const truncatedText = inputText.slice(0, MAX_CHAR)
-        setPostText(truncatedText)
-        setRemainingChar(0)
-      } else {
-        setPostText(inputText)
-        setRemainingChar(MAX_CHAR - inputText.length)
-      }
-  }
-  
+    const inputText = e.target.value;
+    if (inputText.length > MAX_CHAR) {
+      const truncatedText = inputText.slice(0, MAX_CHAR);
+      setPostText(truncatedText);
+      setRemainingChar(0);
+    } else {
+      setPostText(inputText);
+      setRemainingChar(MAX_CHAR - inputText.length);
+    }
+  };
+
   const handleCreatePost = async () => {
-      setLoading(true)
-        try {
-            const res = await fetch("/api/posts/create", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ postedBy: user._id, text: postText, img: imgUrl}),
-            })
+    setLoading(true);
+    try {
+      const res = await fetch("/api/posts/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          postedBy: user._id,
+          text: postText,
+          img: imgUrl,
+        }),
+      });
 
-            const data = await res.json()
-            if(data.error) {
-              showToast("Error", data.error, "error")
-              return
-            }
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
 
-            showToast("Success", data.message, "success")
-            if(username === user.username) {
-              setPost([data, ...posts])
-            }
+      showToast("Success", data.message, "success");
+      if (username === user.username) {
+        setPost([data, ...posts]);
+      }
 
-            onClose()
-        } catch (error) {
-          
-        }
-  }
+      onClose();
+    } catch (error) {
+      showToast("Error", error, "error");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
-    <Button
-    position={"fixed"}
-    bottom={10}
-    right={10}
-    leftIcon={<AddIcon/>}
-    bg={"gray"}
-    onClick={onOpen}
-    >
+      <Button
+        position={"fixed"}
+        bottom={10}
+        right={10}
+        leftIcon={<AddIcon />}
+        bg={"gray"}
+        onClick={onOpen}
+      >
         Post
-    </Button>
+      </Button>
 
-    <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Create Post</ModalHeader>
           <ModalCloseButton />
           <ModalBody p={6}>
-              <FormControl>
-                <Textarea placeholder='post content goes here'
-                          onChange={handleTextChange}
-                          value={postText}
-                />
-                <Text fontSize={"xs"} fontWeight={"bold"} textAlign={"right"} m={"1"} color={"gray.800"}>
-                  {remainingChar} / {MAX_CHAR}
-                </Text>
-                <Input type="file" hidden ref={imageRef} onChange={handleImgChange} />
+            <FormControl>
+              <Textarea
+                placeholder="post content goes here"
+                onChange={handleTextChange}
+                value={postText}
+              />
+              <Text
+                fontSize={"xs"}
+                fontWeight={"bold"}
+                textAlign={"right"}
+                m={"1"}
+                color={"gray.800"}
+              >
+                {remainingChar} / {MAX_CHAR}
+              </Text>
+              <Input
+                type="file"
+                hidden
+                ref={imageRef}
+                onChange={handleImgChange}
+              />
 
-                <BsFillImageFill 
-                    style={{marginLeft: "5px", cursor: "pointer"}}
-                    size={16}
-                    onClick={() => imageRef.current.click()}   
-                />
-              </FormControl>
+              <BsFillImageFill
+                style={{ marginLeft: "5px", cursor: "pointer" }}
+                size={16}
+                onClick={() => imageRef.current.click()}
+              />
+            </FormControl>
 
-              {imgUrl && (
-                <Flex
-                mt={"full"}
-                position={"relative"}
-                >
-                  <Image src={imgUrl} alt="selectImage" />
-                    <CloseButton 
-                    onClick={() => setImgUrl("")}
-                    bg={"gray.800"}
-                    position={"absolute"}
-                    top={2}
-                    right={2}
-                     />
-                </Flex>
-              )}
+            {imgUrl && (
+              <Flex mt={"full"} position={"relative"}>
+                <Image src={imgUrl} alt="selectImage" />
+                <CloseButton
+                  onClick={() => setImgUrl("")}
+                  bg={"gray.800"}
+                  position={"absolute"}
+                  top={2}
+                  right={2}
+                />
+              </Flex>
+            )}
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={handleCreatePost} isLoading>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={handleCreatePost}
+              isLoading
+            >
               Post
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default CreatePosts
+export default CreatePosts;
