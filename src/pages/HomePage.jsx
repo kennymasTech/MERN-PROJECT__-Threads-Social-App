179ca95d4973d858d25180863c4f1727d1c4a477
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-08 10:27:29 +0100

    a

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,32 @@
 import { Button, Flex } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import useShowToast from '../hooks/useShowToast';
+import { set } from 'mongoose';
 
 const HomePage = () => {
+  const showToast = useShowToast()
+  const [ loading, setLoading ] = useState(true)
 
   useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true)
 
-  }, [])
+      try {
+        const response = await fetch("/api/posts/feed");
+
+      } catch (error) {
+        console.log(error);
+        showToast("Error", error, "error")
+        
+      } finally {
+        setLoading(false)
+      }
+    
+    }
+
+    getFeedPosts()
+  }, [showToast])
 
 
 
