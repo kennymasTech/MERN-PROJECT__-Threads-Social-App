57c28b158927519bcaa17c0606ecf0e2eec83783
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-12 09:57:53 +0100

    a

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -12,168 +12,175 @@ import { useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 
 const UserHeader = ({ user }) => {
-	const toast = useToast();
-	const currentUser = useRecoilValue(userAtom);
-   const [following, setFollowing] = useState(user.followers.includes(currentUser?._id))
-   const showToast = useShowToast()
-   const [ updating, setUpdating ] = useState(false)
+  const toast = useToast();
+  const currentUser = useRecoilValue(userAtom);
+  const [following, setFollowing] = useState(
+    user.followers.includes(currentUser?._id)
+  );
+  const showToast = useShowToast();
+  const [updating, setUpdating] = useState(false);
 
-	const copyURL = () => {
-		const currentURL = window.location.href;
-		navigator.clipboard.writeText(currentURL).then(() => {
-			toast({
-				title: "Success",
-				description: "Profile link copied",
-				status: "success",
-				duration: 3000,
-				isClosable: true,
-			});
-		});
-	};
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() => {
+      toast({
+        title: "Success",
+        description: "Profile link copied",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    });
+  };
 
-   const handleFollowUnfollow = async() => {
-      if(!currentUser) {
-        showToast("Error", "Please login to Follow", "error")
-        return
+  const handleFollowUnfollow = async () => {
+    if (!currentUser) {
+      showToast("Error", "Please login to Follow", "error");
+      return;
+    }
+
+    if (updating) return;
+    setUpdating(true);
+
+    try {
+      const res = await fetch(`/api/users/follow/${user._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
       }
 
-      if(updating) return
-      setUpdating(true)
-
-      try {
-         const res = await fetch(`/api/users/follow/${user._id}`, {
-            method: "POST",
-            headers: {
-               "Content-Type": "application/json"
-            }
-         })
-         const data = await res.json()
-         if(data.error) {
-            showToast("Error", data.error, "error")
-            return
-         }
-
-         if(following) {
-            showToast("Success", `You have unfollowed ${user.name}`, "success")
-            user.followers.pop()
-         } else {
-            showToast("Success", `You have followed ${user.name}`, "success")
-            user.followers.push(currentUser?._id)
-         }
-
-         setFollowing(!following)
-         console.log(data)
-         
-      } catch (error) {
-         showToast("Error", error, "error")
-      } finally {
-        setUpdating(false)
+      if (following) {
+        showToast("Success", `You have unfollowed ${user.name}`, "success");
+        user.followers.pop();
+      } else {
+        showToast("Success", `You have followed ${user.name}`, "success");
+        user.followers.push(currentUser?._id);
       }
-   }
 
-	return (
-		<VStack gap={4} alignItems={"start"}>
-			<Flex justifyContent={"space-between"} w={"full"}>
-				<Box>
-					<Text fontSize={"2xl"} fontWeight={"bold"}>
-						{user.name}
-					</Text>
-					<Flex gap={2} alignItems={"center"}>
-						<Text fontSize={"sm"}>{user.username}</Text>
-						<Text
-							fontSize={"xs"}
-							bg={"gray.dark"}
-							color={"gray.light"}
-							p={1}
-							borderRadius={"full"}
-						>
-							threads.net
-						</Text>
-					</Flex>
-				</Box>
-				<Box>
-					{user.profilePic && (
-						<Avatar
-							name={user.name}
-							src={user.profilePic}
-							size={{ base: "md", md: "xl" }}
-						/>
-					)}
-					{!user.profilePic && (
-						<Avatar
-							name={user.name}
-							src="https://tinyurl.com/5n8m9tca"
-							size={{ base: "md", md: "xl" }}
-						/>
-					)}
-				</Box>
-			</Flex>
+      setFollowing(!following);
+      console.log(data);
+    } catch (error) {
+      showToast("Error", error, "error");
+    } finally {
+      setUpdating(false);
+    }
+  };
 
-			<Text>{user.bio}</Text>
+  return (
+    <VStack gap={4} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            {user.name}
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>{user.username}</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          {user.profilePic && (
+            <Avatar
+              name={user.name}
+              src={user.profilePic}
+              size={{ base: "md", md: "xl" }}
+            />
+          )}
+          {!user.profilePic && (
+            <Avatar
+              name={user.name}
+              src="https://tinyurl.com/5n8m9tca"
+              size={{ base: "md", md: "xl" }}
+            />
+          )}
+        </Box>
+      </Flex>
 
-			{currentUser?._id === user._id && (
-				<Link as={RouterLink} to="/update">
-					<Button size={"sm"}> Update Profile</Button>
-				</Link>
-			)}
-			{currentUser?._id !== user._id && (
-				<Link as={RouterLink} >
-					<Button size={"sm"} onClick={handleFollowUnfollow} isLoading={updating}>{following ? "Unfollow" : "Follow"}</Button>
-				</Link>
-			)}
+      <Text>{user.bio}</Text>
 
-			<Flex w={"full"} justifyContent={"space-between"}>
-				<Flex gap={2} alignItems={"center"}>
-					<Text color={"gray.light"}>{user.followers.length} followers</Text>
-					<Box w={1} h={1} bg={"gray.light"} borderRadius={"50%"}></Box>
-					<Link color={"gray.light"}>instagram.com</Link>
-				</Flex>
+      {currentUser?._id === user._id && (
+        <Link as={RouterLink} to="/update">
+          <Button size={"sm"}> Update Profile</Button>
+        </Link>
+      )}
+      {currentUser?._id !== user._id && (
+        <Link as={RouterLink}>
+          <Button
+            size={"sm"}
+            onClick={handleFollowUnfollow}
+            isLoading={updating}
+          >
+            {following ? "Unfollow" : "Follow"}
+          </Button>
+        </Link>
+      )}
 
-				<Flex gap={2}>
-					<Box className="icon-container">
-						<BsInstagram size={24} cursor={"pointer"} />
-					</Box>
-					<Box className="icon-container">
-						<Menu>
-							<MenuButton>
-								<CgMoreO size={24} cursor={"pointer"} />
-							</MenuButton>
-							<Portal>
-								<MenuList bg={"gray.dark"}>
-									<MenuItem color={"white"} bg={"gray.dark"} onClick={copyURL}>
-										Click here
-									</MenuItem>
-								</MenuList>
-							</Portal>
-						</Menu>
-					</Box>
-				</Flex>
-			</Flex>
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>{user.followers.length} followers</Text>
+          <Box w={1} h={1} bg={"gray.light"} borderRadius={"50%"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
 
-			<Flex w={"full"}>
-				<Flex
-					flex={1}
-					borderBottom={"1.5px solid white"}
-					justifyContent={"center"}
-					pb={"3"}
-					cursor={"pointer"}
-				>
-					<Text fontWeight={"bold"}>Thread</Text>
-				</Flex>
+        <Flex gap={2}>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem color={"white"} bg={"gray.dark"} onClick={copyURL}>
+                    Click here
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
 
-				<Flex
-					flex={1}
-					borderBottom={"1.5px solid gray"}
-					justifyContent={"center"}
-					color={"gray.light"}
-					pb={"3"}
-					cursor={"pointer"}
-				>
-					<Text fontWeight={"bold"}> Replies</Text>
-				</Flex>
-			</Flex>
-		</VStack>
-	);
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid white"}
+          justifyContent={"center"}
+          pb={"3"}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Thread</Text>
+        </Flex>
+
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid gray"}
+          justifyContent={"center"}
+          color={"gray.light"}
+          pb={"3"}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}> Replies</Text>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
 };
 
 export default UserHeader;
