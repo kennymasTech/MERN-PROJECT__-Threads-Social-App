dae2b23a823b38400ca9ac62ee73fe8d05c1e32d
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-12 14:05:08 +0100

    a

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -149,7 +149,16 @@ const PostPage = () => {
         <Button>Get</Button>
       </Flex>
 
-      <Divider my={4} />
+      <Divider my={4} >
+		{post.replies.map(reply => {
+			<Comment 
+			key={reply._id}
+			reply={reply}
+			/>
+		})}
+	  </Divider>
+
+	  
 
       {/* <Comments username={'Ryan Florence'} likes={12} createdAt={'2 min ago'} userAvatar={'https://bit.ly/ryan-florence'} comment={"Keep it up!!"} /> */}
     </>
