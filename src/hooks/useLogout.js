7d60cae446c85ce21bcc508a2f377df1d9d6e609
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-13 09:36:56 +0100

    a

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,34 @@
+import { useRecoilValue } from "recoil"
+import userAtom from "../atoms/userAtom"
+import useShowToast from "./useShowToast"
+
+
+
+const useLogout = () => {
+    const setUser = useRecoilValue(userAtom)
+    const showToast = useShowToast()
+  
+    const logout = async () => {
+        try {
+            const res = await fetch("/api/users/logout", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+
+            const data = await res.json()
+
+            if(data.error) {
+                showToast("Error", data.error, "error")
+                return;
+            }
+            localStorage.removeItem("user-threads")
+            setUser(null)
+        } catch (error) {
+            showToast("Error", error, "error")
+        }
+    }
+}
+
+export default useLogout
