ac0d665c89933c0d13f70708259abba0e7f01260
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-13 09:52:19 +0100

    a

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -10,7 +10,7 @@ const useLogout = () => {
   
     const logout = async () => {
         try {
-            const res = await fetch("/api/users/logout", {
+            const res = await fetch("api/users/logout", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
